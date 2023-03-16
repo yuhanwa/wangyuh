70a2d1168a6cb795c6c6a35f9cc6e904d0c07e64
YUHAN WANG (Etudiant) <wangyuh@ur.univ-rouen.fr>
2023-03-16 10:43:07 +0100

    ajoute server bienvenue

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 let express =require ('express') ;
 let app = express() ;
 let port = 3000;
+
+app.use( express. static(__dirname + '/www')) ;
+
+
 app . listen ( port , () => {
 console.log( 'Le serveur est en route') ;
 console.log ( `Serveur listening at http://
 localhost:${port}`) ;
 })
 app.get('/',( req , res, next) => {
-res.send ( 'Bonjour !') ;
+res.sendFile ( '/www/index.html') ;
 }) ;
